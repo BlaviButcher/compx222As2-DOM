a1d2e88a610d5429b81c6f61df6bdb5fbb3c7900
BlaviButcher <rileydouglas@outlook.com>
2021-08-12 09:34:22 +1200

    refactor paper.js

--- js/part1/paper.js
+++ js/part1/paper.js
@@ -35,13 +35,33 @@ function drawSquares(userInput) {
     }
 }
 
+/**
+ * alternate colors depending on player turn
+ * @param {Element} square 
+ * @param {boolean} turn true for player 1 false for player 2
+ */
+function colorSquare(square, turn) {
+    if (isPlayer1Turn === true)
+        square.style.backgroundColor = 'blue';
+    else square.style.backgroundColor = 'red';
+}
 
+/**
+ * changes current players turn and resets selection to first
+ */
+function changeTurns() {
+    isPlayer1Turn = !isPlayer1Turn;
+    selectionInfo.selection = 1;
+}
+
+//DEBUG
 let userInput = "6";
-let isPlayer1Turn = true;
 
+
+let isPlayer1Turn = true;
 let selectionInfo = {
     firstSelectionIndex: -1,
-    isFirstSelection: true
+    selection: 1
 }
 
 // error handling for userInput
@@ -63,13 +83,12 @@ function isAdjacent(square, index) {
     return (square.index - 1 == index) || (square.index + 1 == index);
 }
 
+// Get container that will hold squares
 let gameContainer = document.getElementById("paper-container");
-
 drawSquares(userInput);
 
-
+// get squares
 let squares = document.getElementsByClassName("square");
-console.log(squares);
 
 // using a for loop rather than a foreach as we need an index, becausewant to be able to store the placement 
 // of square
@@ -80,51 +99,27 @@ for (let i = 0; i < squares.length; i++) {
     // store placement in strip
     square.index = i;
     square.addEventListener("click", () => {
-        if (square.reachable === true) {
-            // DEBUG
-            console.log(`first index = ${selectionInfo.firstSelectionIndex}`);
-            console.log(square.index);
+        if (square.reachable == true) {
 
-            // if players second pick check that it is adjacent
-            if (!selectionInfo.isFirstSelection) {
-                if (!isAdjacent(square, selectionInfo.firstSelectionIndex)) {
-                    alert("Your second choice must be adjacent to the" +
-                        " first");
-                    return;
-                }
-
-                // DEBUG
-                console.log(square.index - 1 != selectionInfo.firstSelectionIndex);
-                console.log(square.index + 1 != selectionInfo.firstSelectionIndex);
-
+            // if players second pick and chosen square is not adjacent
+            if (selectionInfo.selection == 2 && !isAdjacent(square, selectionInfo.firstSelectionIndex)) {
+                alert("Your second choice must be adjacent to the" +
+                    " first");
+                return;
             }
 
             colorSquare(square, isPlayer1Turn);
             square.reachable = false;
 
             // if second selection of players turn
-            if (!selectionInfo.isFirstSelection) {
+            if (selectionInfo.selection == 2) {
                 changeTurns();
                 return;
             }
             selectionInfo.firstSelectionIndex = square.index;
-            selectionInfo.isFirstSelection = !selectionInfo.isFirstSelection;
+            selectionInfo.selection++;
         }
     })
 }
-/**
- * alternate colors depending on player turn
- * @param {Element} square 
- * @param {boolean} turn true for player 1 false for player 2
- */
-function colorSquare(square, turn) {
-    if (isPlayer1Turn === true)
-        square.style.backgroundColor = 'blue';
-    else square.style.backgroundColor = 'red';
-}
 
-function changeTurns() {
-    isPlayer1Turn = !isPlayer1Turn;
-    selectionInfo.isFirstSelection = !selectionInfo.isFirstSelection;
-}
 
