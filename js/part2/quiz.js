cdbbf471e1f9c376521bfbe44157b76e74723b23
BlaviButcher <rileydouglas@outlook.com>
2021-08-13 08:57:58 +1200

    directory update

---
+++ js/part2/quiz.js
@@ -0,0 +1,11 @@
+let questions = [
+    {
+        question: "Who can require you to undergo a breath screening test?",
+        image: "url(../../assets/part2/beer.jpg",
+        option1: "Your Mum",
+        option2: "A Police Officer",
+        option3: "Your neighbor",
+        option4: "A man welding a gun",
+        answer: option2
+    }
+];
