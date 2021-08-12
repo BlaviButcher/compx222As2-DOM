9199c7d12a7212f3e3fbbc24823e3e614ffcbd8a
BlaviButcher <rileydouglas@outlook.com>
2021-08-13 10:36:07 +1200

    added all questions

--- js/part2/quiz.js
+++ js/part2/quiz.js
@@ -6,20 +6,135 @@ let questions = [
             "Your Mum",
             "A Police Officer",
             "Your neighbor",
-            "A man welding a gun",
+            "A man welding a gun"
         ],
 
         answer: "2"
+    },
+
+    {
+        question: "When may you use the horn on your vehicle?",
+        image: "../../assets/part2/img/car-horn.jpg",
+        options: [
+            "To warn traffic or pedestrians",
+            "To say g'day to old mate",
+            "To express rage",
+            "You should never use the horn"
+        ],
+        answer: "1"
+    },
+
+    {
+        question: "What must you do when red lights are flashing at a railway crossing?",
+        image: "../../assets/part2/img/train-track.jpg",
+        options: [
+            "Ignore it, probably faulty",
+            "Stop in the middle of the crossing",
+            "Stop before the crossing",
+            "Turn around"
+        ],
+        answer: "3"
+    },
+
+    {
+        question: "When must you signal when you're turning right?",
+        image: "../../assets/part2/img/car-indicator.jpg",
+        options: [
+            "Atleast 6 seconds before turning",
+            "Atleast 4 seconds before turning",
+            "Atleast 5 seconds before turning",
+            "Atleast 3 seconds before turning"
+        ],
+        answer: "4"
+    },
+
+    {
+        question: "What is the speed limit when passing an accident site?",
+        image: "../../assets/part2/img/car-accident.jpg",
+        options: [
+            "40km/h or less",
+            "20km/h or less",
+            "50km/h or less",
+            "30km/h or less"
+        ],
+        answer: "2"
+    },
+
+    {
+        question: "At night, when must your dip the headlights of your vehicle?",
+        image: "../../assets/part2/img/car-headlights.jpg",
+        options: [
+            "Never!",
+            "When you see animals",
+            "When parked",
+            "All the time"
+        ],
+        answer: "3"
+    },
+
+    {
+        question: "What does a flashing yellow light mean at an traffic light intersection?",
+        image: "../../assets/part2/img/traffic-light.jpg",
+        options: [
+            "Traffic light not working",
+            "Slow down",
+            "Speed up",
+            "Stop"
+        ],
+        answer: "1"
+    },
+
+    {
+        question: "At an intersection what does a police officer's directions overrule?",
+        image: "../../assets/part2/img/police-officer.jpg",
+        options: [
+            "The give way rules",
+            "Road signs",
+            "Traffic signals",
+            "All of the above"
+        ],
+        answer: "4"
+    },
+
+    {
+        question: "When should you apply the 4 second rule?",
+        image: "../../assets/part2/img/clock.jpg",
+        options: [
+            "All the time",
+            "There is no such thing",
+            "In bad weather",
+            "When following motorcycles"
+        ],
+        answer: "3"
+    },
+
+    {
+        question: "If someone is hurt in a crash, how long does the driver have to call it in?",
+        image: "../../assets/part2/img/building-crash.jpg",
+        options: [
+            "1 hours",
+            "12 hours",
+            "24 hours",
+            "48 hours"
+        ],
+        answer: "4"
     }
+
 ];
 
-let questionNumber = 1;
-let image = document.getElementsByTagName("img")[0];
-let buttons = document.getElementsByClassName("answer");
 
 
-displayQuestion();
-function displayQuestion() {
+document.getElementById("next-question").addEventListener("click", () => {
+    displayQuestion(++questionNumber);
+});
+
+
+/**
+ * 
+ * @param {int} questionNumber current question
+ */
+function displayQuestion(questionNumber) {
+    if (questionNumber == 10) quizDone();
     // set image for current question
     image.src = questions[questionNumber - 1].image;
 
@@ -28,6 +143,14 @@ function displayQuestion() {
         buttons[i].textContent = questions[questionNumber - 1].options[i];
 }
 
+function quizDone() {
+    document.getElementsByTagName("body")[0].innerHTML = "";
+}
 
 
 
+let questionNumber = 1;
+let image = document.getElementsByTagName("img")[0];
+let buttons = document.getElementsByClassName("answer");
+
+displayQuestion(questionNumber);
