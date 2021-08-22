612b367f993c2628c7fe8323cc2134252a2787e8
BlaviButcher <rileydouglas@outlook.com>
2021-08-23 11:19:05 +1200

    incorrect answer logic improved

--- js/part2/quiz.js
+++ js/part2/quiz.js
@@ -140,6 +140,10 @@ let questions = [
         </main>
     </div>
 */
+
+// I built this purely for the sake of getting a better grasp of DOM api.
+// I learned about JSX which was cool, but can't assume marker is using Babel
+
 function buildMainScreen() {
     let body = document.getElementsByTagName("body")[0];
     // clear body
@@ -170,6 +174,7 @@ function buildMainScreen() {
     let answerContainer = document.createElement("div");
     answerContainer.setAttribute("id", "answer-container");
 
+    // create buttons
     for (let i = 0; i < 4; i++) {
         let button = document.createElement("button");
         button.setAttribute("class", "answer");
@@ -188,10 +193,30 @@ function buildMainScreen() {
     main.appendChild(footer);
 }
 
+function buildAnswerPage() {
+    // remove content of answer container
+    let answerCont = document.getElementById("answer-container");
+    answerCont.innerHTML = "";
+
+    let yourAnswer = document.createElement("div");
+    yourAnswer.setAttribute("class", "answer-page-io");
+
+
+    let correctAnswer = document.createElement("div");
+    correctAnswer.setAttribute("class", "answer-page-io");
+
+    yourAnswer.setAttribute("id", "your-answer");
+    answerCont.appendChild(yourAnswer);
+
+    correctAnswer.setAttribute("id", "correct-answer");
+    answerCont.appendChild(correctAnswer);
+
+}
 
 
 function nextQuestion() {
-    displayQuestion(++questionNumber);
+    questionNumber++;
+    displayQuestion();
 }
 
 
@@ -199,44 +224,72 @@ function nextQuestion() {
  * 
  * @param {int} questionNumber current question
  */
-function displayQuestion(questionNumber) {
-    if (questionNumber == 11) {
-        quizDone();
-        document.getElementById("next-question").removeEventListener("click", nextQuestion);
-        return;
+function displayQuestion() {
+
+    if (questionNumber > QUESTION_AMOUNT) {
+
+        // if answers are done return
+        if (isAnswerSheet) {
+            document.getElementById("next-question").removeEventListener("click", nextQuestion);
+            return;
+        }
+        // else move onto answer logic
+        buildAnswerPage();
+        isAnswerSheet = true;
+        // reset question number
+        questionNumber = 1;
+        // will now move through all answers
+
     }
+    // index that will correspond to data in arrays 
+    // given the current question
+    let index = questionNumber - 1;
+
+
+
+    // If answers logic - display user answer and correct answer
+    if (isAnswerSheet) {
+        console.log(questionNumber);
+        console.log(userAnswers[index]);
+
+        console.log(userAnswers[index].yourAnswer);
+        console.log(userAnswers[index].correctAnswer);
+
+        // if is correct answer
+        if (userAnswers[index].givenAnswer == userAnswers[index].correctAnswer) {
+            console.log("same answers");
+            questionNumber++;
+            displayQuestion();
+            return;
+        }
+        let yourAnswer = document.getElementById("your-answer");
+        let correctAnswer = document.getElementById("correct-answer");
+
+        yourAnswer.textContent = userAnswers[questionNumber - 1].givenAnswer;
+        correctAnswer.textContent = userAnswers[questionNumber - 1].correctAnswer;
+
+        // normal question logic
+    } else {
+        // Fill buttons with options from current question
+        for (let i = 0; i < buttons.length; i++) {
+            // update button
+            buttons[i].textContent = questions[questionNumber - 1].options[i];
+            buttons[i].index = i;
+            buttons[i].style.color = 'black';
+
+            // set event handlers for buttons. Correct answer has different answer
+            if (i == questions[questionNumber - 1].answer - 1) {
+                buttons[i].addEventListener("click", correctAnswer);
+            } else buttons[i].addEventListener("click", incorrectAnswer);
+        }
+    }
+
     // set image for current question
-    image.src = questions[questionNumber - 1].image;
-    question.textContent = questions[questionNumber - 1].question;
-    // Fill buttons with options from current question
-    for (let i = 0; i < buttons.length; i++) {
-        // update button
-        buttons[i].textContent = questions[questionNumber - 1].options[i];
-        buttons[i].index = i;
-        buttons[i].style.color = 'black';
-
-        // if i is the same index as the answer
-        if (i == questions[questionNumber - 1].answer - 1) {
-            buttons[i].addEventListener("click", correctAnswer);
-        } else buttons[i].addEventListener("click", incorrectAnswer);
-    }
+    image.src = questions[index].image;
+    // set question text
+    question.textContent = questions[index].question;
 
 
-}
-
-function quizDone() {
-    let body = document.getElementsByTagName("body")[0];
-    body.innerHTML = "";
-    for (let answer of incorrectAnswers) {
-        let givenAnswer = document.createElement("p");
-        givenAnswer.textContent = `Your answer was: ${answer.givenAnswer}`;
-
-        let correctAnswer = document.createElement("p");
-        correctAnswer.textContent = `Correct answer: ${answer.correctAnswer}`;
-
-        body.appendChild(givenAnswer);
-        body.appendChild(correctAnswer);
-    }
 }
 
 /**
@@ -244,31 +297,39 @@ function quizDone() {
  * @param {Element} button event caller 
  */
 function incorrectAnswer(button) {
-    // get correct answer index
-    let answer = questions[questionNumber - 1].answer;
+
     // add given answer and correct answer to array
-    incorrectAnswers.push({
-        // get contents of given answer
-        givenAnswer: questions[questionNumber - 1].options[button.target.index],
-        // get correct answer
-        correctAnswer: questions[questionNumber - 1].options[answer - 1]
-    });
-    console.log(questions[questionNumber - 1].options[button.target.index]);
-    console.log(questions[questionNumber - 1].options[answer - 1]);
+    storeUserAnswer(button);
+    // make selected button red
     button.target.style.color = 'red';
+    // get rid of click events (all events - only has click)
     removeButtonListeners();
 
 }
 
 /**
- * Correct answer handler
+ * 
+ * @param {Element} button 
  */
 function correctAnswer(button) {
     correctAnswers++;
+    storeUserAnswer(button);
     button.target.style.color = 'green';
     removeButtonListeners();
 }
 
+// save user anser to array
+function storeUserAnswer(button) {
+
+    // get correct answer index
+    let answer = questions[questionNumber - 1].answer;
+    userAnswers.push({
+        // get contents of given answer
+        givenAnswer: questions[questionNumber - 1].options[button.target.index],
+        // get correct answer
+        correctAnswer: questions[questionNumber - 1].options[answer - 1],
+    });
+}
 
 /**
  * Drops the click event on all buttons
@@ -285,14 +346,22 @@ function removeButtonListeners() {
 
 
 
-buildMainScreen();
+// buildMainScreen();
 
 let questionNumber = 1;
-let image = document.getElementsByTagName("img")[0];
-let buttons = document.getElementsByClassName("answer");
+const image = document.getElementsByTagName("img")[0];
+const buttons = document.getElementsByClassName("answer");
 let question = document.getElementById("question");
-let incorrectAnswers = [];
+const userAnswers = [];
 let correctAnswers = 0;
+// denotes whether we are going through answer logic
+let isAnswerSheet = false;
+let isQuizDone = false;
+const QUESTION_AMOUNT = 3;
 
 document.getElementById("next-question").addEventListener("click", nextQuestion);
+
+
+
 displayQuestion(questionNumber);
+
